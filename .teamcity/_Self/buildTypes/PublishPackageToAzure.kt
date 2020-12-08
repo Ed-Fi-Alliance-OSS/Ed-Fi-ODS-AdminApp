4398812060f07022c3662201a8dcef7613ee9fe3
Suganya <srajendran@certicasolutions.com>
2020-12-08 13:25:27 -0600

    Rename build type object

--- .teamcity/_Self/buildTypes/PublishPackageToAzure.kt
+++ .teamcity/_Self/buildTypes/PublishPackageToAzure.kt
@@ -10,7 +10,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 
-object PublishPackage : BuildType ({
+object PublishPackageToAzure : BuildType ({
     name = "Publish Package to Azure"
     description = "Publishes NuGet package to the Azure feed"
 
