0138f0c62e8371e4784917d9c45db3f9d4273867
AndonyNS <andony91@gmail.com>
2022-02-18 15:29:00 -0600

    [AA-1428] [AA-1429] First time setup test

--- Application/EdFi.Ods.AdminApp.E2E.Tests/features/models/firstTimeSetupPage.ts
+++ Application/EdFi.Ods.AdminApp.E2E.Tests/features/models/firstTimeSetupPage.ts
@@ -1,11 +1,21 @@
 import { AdminAppPage } from "./adminAppPage";
 
 export class FirstTimeSetupPage extends AdminAppPage {
+    continueBtn = "a#finish-setup-link";
+
     path(): string {
         return `${this.url}/Setup/FirstTimeSetup`;
     }
 
     hasTitle() {
         return this.hasText({ text: "Additional Setup Required" });
+    }
+
+    async continue(url: string): Promise<void> {
+        await Promise.all([this.clickContinue(), this.page.waitForNavigation({ url })]);
+    }
+
+    private async clickContinue(): Promise<void> {
+        await this.page.click(this.continueBtn);
     }
 }
