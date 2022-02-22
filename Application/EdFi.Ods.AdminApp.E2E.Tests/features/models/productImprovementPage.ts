25bc57e479742a6b2cd58d069f9cfd8408361b44
AndonyNS <andony91@gmail.com>
2022-02-22 17:31:25 -0600

    [AA-1428] Refactor to avoid having two methods to verify pages on first time setup

--- Application/EdFi.Ods.AdminApp.E2E.Tests/features/models/productImprovementPage.ts
+++ Application/EdFi.Ods.AdminApp.E2E.Tests/features/models/productImprovementPage.ts
@@ -1,6 +1,15 @@
 import { AdminAppPage } from "./adminAppPage";
 
 export class ProductImprovementPage extends AdminAppPage {
+
+    //Override
+    get isOnPage(): boolean {
+        const currentURL = this.page.url();
+        const baseURL = currentURL.substring(0, currentURL.indexOf("?"));
+        const URL = baseURL === "" ? currentURL : baseURL;
+        return URL === this.path() || URL === this.firstTimePath();
+    }
+
     path(): string {
         return `${this.url}/ProductImprovement/EnableProductImprovement`;
     }
@@ -11,9 +20,5 @@ export class ProductImprovementPage extends AdminAppPage {
 
     async hasPageTitle(): Promise<boolean> {
         return await this.hasText({ text: "Enable Product Improvement", selector: "h2" });
-    }
-
-    isOnFirstTimeProductImprovementPage(): boolean {
-        return this.page.url() === this.firstTimePath();
     }
 }
