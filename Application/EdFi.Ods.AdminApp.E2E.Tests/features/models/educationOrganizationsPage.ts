4f9e232b86e9c999195aa1023999d5af42d08d39
AndonyNS <andony91@gmail.com>
2022-02-23 10:30:58 -0600

    [AA-1425] [AA-861] Delete local education agency

--- Application/EdFi.Ods.AdminApp.E2E.Tests/features/models/educationOrganizationsPage.ts
+++ Application/EdFi.Ods.AdminApp.E2E.Tests/features/models/educationOrganizationsPage.ts
@@ -29,10 +29,12 @@ export class EducationOrganizationsPage extends AdminAppPage {
 
     confirmationMessages = {
         leaAdded: "Organization Added",
+        leaDeleted: "Organization Removed",
     };
 
     modalTitleMessages = {
         AddLEA: "Add Local Education Agency",
+        DeleteLEA: "Delete Local Education Agency",
     };
 
     path(): string {
@@ -54,10 +56,24 @@ export class EducationOrganizationsPage extends AdminAppPage {
         await this.page.click(this.addNewLeaBtn);
     }
 
-    async hasLEAModalTitle(): Promise<boolean> {
+    async hasAddLEAModalTitle(): Promise<boolean> {
         return (
             (await this.page.locator(this.modalTitleSection).textContent()) === this.modalTitleMessages.AddLEA
         );
+    }
+
+    async hasDeleteLEAModalTitle(): Promise<boolean> {
+        return (
+            (await this.page.locator(this.modalTitleSection).textContent()) ===
+            this.modalTitleMessages.DeleteLEA
+        );
+    }
+
+    async hasDeleteModalConfirmationMessage(): Promise<boolean> {
+        return this.hasText({
+            text: `Are you sure you want to permanently delete ${this.leaFormValues.name}`,
+            selector: ".modal.fade.in .modal-body",
+        });
     }
 
     async fillLEAForm(): Promise<void> {
@@ -79,18 +95,38 @@ export class EducationOrganizationsPage extends AdminAppPage {
         return this.hasText({ text: this.leaFormValues.name, selector: this.nameOnList });
     }
 
-    async confirmationAppears(): Promise<boolean> {
+    async addedConfirmationAppears(): Promise<boolean> {
         return (await this.getToastMessage()) === this.confirmationMessages.leaAdded;
     }
 
-    async deleteLEA(): Promise<void> {
-        await this.hasPageTitle();
-        await this.clickDelete();
-        await this.clickConfirmDelete();
+    async deletedConfirmationAppears(): Promise<boolean> {
+        return (await this.getToastMessage()) === this.confirmationMessages.leaDeleted;
     }
 
     async clickDelete(): Promise<void> {
         await this.page.click(this.deleteLEABtn);
+    }
+
+    async deleteLEA() {
+        await Promise.all([
+            this.clickConfirmDelete(),
+            this.waitForResponse({ url: "/EducationOrganizations/DeleteLocalEducationAgency" }),
+        ]);
+    }
+
+    async deleteLEAFullSteps(): Promise<void> {
+        await this.hasPageTitle();
+        await this.clickDelete();
+        await this.deleteLEA();
+    }
+
+    async addLocalEducationAgencyFullSteps(): Promise<void> {
+        await this.navigate();
+        await this.addNewLEA();
+        await this.fillLEAForm();
+        await this.saveLEAForm();
+        await this.addedConfirmationAppears();
+        await this.waitForListLoad();
     }
 
     private async clickConfirmDelete(): Promise<void> {
