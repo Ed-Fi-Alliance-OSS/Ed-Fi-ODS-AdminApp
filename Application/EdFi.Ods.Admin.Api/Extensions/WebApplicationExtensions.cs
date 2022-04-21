a95fe859a008a92d05035ad514232fc621eded1c
CSR2017 <suganya@imanisoftware.com>
2022-04-21 17:30:16 -0500

    Add base route builder interface, which will be implemented by all the features like Vendor, Application..WebApplicationExtension is for adding common platform for facilitating easy way to add new route builders

---
+++ Application/EdFi.Ods.Admin.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Ods.Admin.Api.Features;
+using System.Reflection;
+
+namespace EdFi.Ods.Admin.Api.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void UseRouteBuilders(this WebApplication application)
+        {
+            var type = typeof(IRouteBuilderBase);
+            var routeBuilderTypes = Assembly.GetExecutingAssembly().GetTypes()                
+                .Where(p => type.IsAssignableFrom(p) && p.IsClass);
+
+            var routeBuilders = new List<IRouteBuilderBase>();
+
+            foreach (var routeBuilderType in routeBuilderTypes)
+            {
+                var routeBuilder = Activator.CreateInstance(routeBuilderType) as IRouteBuilderBase;
+                if(routeBuilder != null)                    
+                  routeBuilders.Add(routeBuilder);
+            }
+
+            application.UseEndpoints(endpoints => {
+
+                foreach (var routeBuilder in routeBuilders)
+                {
+                    routeBuilder.AddEndPoints(endpoints);
+                }
+            });   
+        }
+    }
+}
