bf9cacbb979fde6f714d8b6a75359b6f7ee07ea7
Patrick McVeety-Mill <patrick.mcveety-mill@accenture.com>
2022-04-29 10:20:18 -0500

    append license to new files

--- Application/EdFi.Ods.AdminApp.E2E.Tests/features/management/setup.ts
+++ Application/EdFi.Ods.AdminApp.E2E.Tests/features/management/setup.ts
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 import { Before, BeforeAll, ITestCaseHookParameter, setDefaultTimeout } from "@cucumber/cucumber";
 import { chromium, Browser, BrowserContext, Page } from "playwright";
 import { ModelResolver } from "../models/modelResolver";
