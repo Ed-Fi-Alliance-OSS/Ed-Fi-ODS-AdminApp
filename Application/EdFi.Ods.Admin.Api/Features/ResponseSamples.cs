f48c68b48dd2e79a54c45c5d208c3b2993e788d9
Patrick McVeety-Mill <patrick.mcveety-mill@accenture.com>
2022-05-04 09:33:16 -0500

    move samples out of vendor namespace

--- Application/EdFi.Ods.Admin.Api/Features/ResponseSamples.cs
+++ Application/EdFi.Ods.Admin.Api/Features/ResponseSamples.cs
@@ -3,7 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-namespace EdFi.Ods.Admin.Api.Features.Vendors;
+namespace EdFi.Ods.Admin.Api.Features;
 
 public class SamplesRouteBuilder : IFeature
 {
