fc785aef8a04af5ce1f3a8a15de9ea325d41e978
AndonyNS <andony91@gmail.com>
2022-05-30 09:59:10 -0600

    Adding workaround support for Alpine 15

--- Application/EdFi.Ods.AdminApp.E2E.Tests/gh-action-setup/set-versions.sh
+++ Application/EdFi.Ods.AdminApp.E2E.Tests/gh-action-setup/set-versions.sh
@@ -15,3 +15,10 @@ echo "Using $DB Version: $DB_VERSION"
 sed -i "s/ENV VERSION=\"[0-9].[0-9].[0-9]\"/ENV VERSION=\"$WEB_VERSION\"/g" ods-docker/Web-Ods-AdminApp/Alpine/pgsql/Dockerfile
 
 sed -i "s/ENV ADMINAPP_DATABASE_VERSION=\"[0-9].[0-9].[0-9]\"/ENV ADMINAPP_DATABASE_VERSION=\"$DB_VERSION\"/g" ods-docker/DB-Admin/Alpine/pgsql/Dockerfile
+
+# This must be removed once ODS-Docker supports .NET 6.
+# These are workarounds to support Alpine 15
+
+sed -i "s/core\/aspnet@sha256:[a-z0-9]\+/aspnet:6.0-alpine/g" ods-docker/Web-Ods-AdminApp/Alpine/pgsql/Dockerfile
+
+sed -i "s/RUN apk --no-cache add unzip=~6.0 dos2unix=~7.4 bash=~5.1 gettext=~0.21 postgresql-client=~13.6 jq=~1.6 icu=~67.1 curl=~7.79/RUN apk --no-cache add unzip=~6.0 dos2unix=~7.4 bash=~5.1 gettext=~0.21 postgresql13-client=~13.6 jq=~1.6 icu=~69.1 curl=~7.80/g" ods-docker/Web-Ods-AdminApp/Alpine/pgsql/Dockerfile
