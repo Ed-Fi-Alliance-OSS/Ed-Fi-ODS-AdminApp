c39067f013fe6c7756728e60d5fa77561d6f1b2c
Patrick McVeety-Mill <patrick.mcveety-mill@accenture.com>
2022-09-09 12:40:59 -0500

    add flag to support full regex

--- Application/EdFi.Ods.AdminApp.E2E.Tests/gh-action-setup/set-versions.sh
+++ Application/EdFi.Ods.AdminApp.E2E.Tests/gh-action-setup/set-versions.sh
@@ -12,9 +12,9 @@ DB_PACKAGE=$(nuget list $DB -Source $NUGET_URL)
 DB_VERSION=$(echo $DB_PACKAGE| cut -d' ' -f 2)
 echo "Using $DB Version: $DB_VERSION"
 
-sed -i "s/ENV ADMINAPP_DATABASE_VERSION=\"[0-9]+.[0-9]+.[0-9]+\"/ENV ADMINAPP_DATABASE_VERSION=\"$DB_VERSION\"/w changelog.txt" ods-docker/DB-Admin/Alpine/pgsql/Dockerfile
+sed -i -E "s/ENV ADMINAPP_DATABASE_VERSION=\"[0-9]+.[0-9]+.[0-9]+\"/ENV ADMINAPP_DATABASE_VERSION=\"$DB_VERSION\"/w changelog.txt" ods-docker/DB-Admin/Alpine/pgsql/Dockerfile
 
-sed -i "s/ENV VERSION=\"[0-9]+.[0-9]+.[0-9]+\"/ENV VERSION=\"$WEB_VERSION\"/w changelog.txt" ods-docker/Web-Ods-AdminApp/Alpine/pgsql/Dockerfile
+sed -i -E "s/ENV VERSION=\"[0-9]+.[0-9]+.[0-9]+\"/ENV VERSION=\"$WEB_VERSION\"/w changelog.txt" ods-docker/Web-Ods-AdminApp/Alpine/pgsql/Dockerfile
 
 if [ -s changelog.txt ]; then
     echo "Files updated"
