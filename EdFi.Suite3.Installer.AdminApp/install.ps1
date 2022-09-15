39867b4df15b2454c0a2b854e2bd06886cfa5e3c
Patrick McVeety-Mill <patrick.mcveety-mill@accenture.com>
2022-09-15 15:01:01 -0500

    update admin app installer version to new release

--- EdFi.Suite3.Installer.AdminApp/install.ps1
+++ EdFi.Suite3.Installer.AdminApp/install.ps1
@@ -73,7 +73,7 @@ $p = @{
     ToolsPath = "C:/temp/tools"
     DbConnectionInfo = $dbConnectionInfo
     OdsApiUrl = ""
-    PackageVersion = '2.4.35'
+    PackageVersion = '2.4.48'
     AdminAppFeatures = $adminAppFeatures
 }
 
