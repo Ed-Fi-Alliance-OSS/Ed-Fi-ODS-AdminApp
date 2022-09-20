e36848014beeacf2fce2536c9ac69b7541a72e4f
saa14 <saagar.minocha@accenture.com>
2022-09-21 00:13:37 +0530

    Added IdentitySettings section to appsettings.json. Added corresponding classes/constants for IdentitySettings.

---
+++ Application/EdFi.Ods.AdminApp.Management/Helpers/IdentitySettingsConstants.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.Ods.AdminApp.Management.Helpers
+{
+    public class IdentitySettingsConstants
+    {
+        public const string EntityFrameworkIdentityType = "EntityFramework";
+        public const string OpenIdIdentityType = "OpenId";
+        public const string OpenIdIdentityControllerName = "OpenIdConnect";
+        public const string EntityFrameworkIdentityControllerName = "Identity";
+    }
+}
