5b62926abb1e58bccf2f149ce77648222956bc5f
saa14 <saagar.minocha@accenture.com>
2022-09-21 00:22:59 +0530

    Added OpenIdConnectController and corresponding login/logout related actions

---
+++ Application/EdFi.Ods.AdminApp.Web/Controllers/OpenIdConnectController.cs
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EdFi.Ods.AdminApp.Web.ActionFilters;
+using EdFi.Ods.AdminApp.Management.Helpers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace EdFi.Ods.AdminApp.Web.Controllers
+{
+    [BypassSetupRequiredFilter, BypassInstanceContextFilter]
+    public class OpenIdConnectController : ControllerBase
+    {
+        private readonly IdentitySettings _identitySettings;
+
+        public OpenIdConnectController(IOptions<IdentitySettings>
+            identitySettings)
+        {
+            _identitySettings = identitySettings.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/Home/Index")
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(_identitySettings.OpenIdSettings.AuthenticationScheme);
+            }
+            
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return Redirect(_identitySettings.OpenIdSettings.UserProfileUri);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
