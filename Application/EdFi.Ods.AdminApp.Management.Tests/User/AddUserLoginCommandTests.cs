98869363054e41b309130a65082af5bb81d70863
saa14 <saagar.minocha@accenture.com>
2022-09-23 00:41:57 +0530

    Added tests for AddUserLoginCommand and OpenIdConnectLoginService.

---
+++ Application/EdFi.Ods.AdminApp.Management.Tests/User/AddUserLoginCommandTests.cs
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EdFi.Ods.AdminApp.Management.User;
+using EdFi.Ods.AdminApp.Management.Database;
+using EdFi.Ods.AdminApp.Web.Models.ViewModels.User;
+using NUnit.Framework;
+using Shouldly;
+using static EdFi.Ods.AdminApp.Management.Tests.Testing;
+using static EdFi.Ods.AdminApp.Management.Tests.User.UserTestSetup;
+
+namespace EdFi.Ods.AdminApp.Management.Tests.User
+{
+    [TestFixture]
+    public class AddUserLoginCommandTests
+    {
+        [Test]
+        public async Task ShouldAddUserLogin()
+        {
+            var newUserLogin = new AddUserLoginModel
+            {
+                UserEmail = "test@test.com",
+                LoginProvider = "test_oidc",
+                ProviderDisplayName = "test_oidc",
+                ProviderKey = "test_provider_key"
+            };
+
+            await ScopedAsync<AdminAppIdentityDbContext>(async context =>
+            {
+                var command = new AddUserLoginCommand(context);
+
+                var identityUserId = await command.Execute(newUserLogin);
+
+                var addedUserLogin = context.UserLogins.SingleOrDefault(x => x.UserId == identityUserId);
+                addedUserLogin.ShouldNotBeNull();
+                addedUserLogin.ProviderKey.ShouldBe(newUserLogin.ProviderKey);
+                addedUserLogin.LoginProvider.ShouldBe(newUserLogin.LoginProvider);
+                addedUserLogin.ProviderDisplayName.ShouldBe(newUserLogin.ProviderDisplayName);
+            });
+        }
+
+        [Test]
+        public void ShouldNotAddUserLoginIfRequiredFieldsEmpty()
+        {
+            var newUserLogin = new AddUserLoginModel();
+
+            Scoped<AdminAppIdentityDbContext>(identity =>
+            {
+                var validator = new AddUserLoginModelValidator(identity);
+                var validationResults = validator.Validate(newUserLogin);
+                validationResults.IsValid.ShouldBe(false);
+                validationResults.Errors.Select(x => x.ErrorMessage).ShouldBe(new List<string>
+                {
+                    "'User Email' must not be empty.",
+                    "'Login Provider' must not be empty.",
+                    "'Provider Key' must not be empty."
+                });
+            });
+        }
+
+        [Test]
+        public void ShouldNotAddUserLoginIfEmailNotValid()
+        {
+            var newUserLogin = new AddUserLoginModel
+            {
+                UserEmail = "not-a-valid-email",
+                LoginProvider = "test_oidc",
+                ProviderKey = "test_provider_key"
+            };
+
+            Scoped<AdminAppIdentityDbContext>(identity =>
+            {
+                var validator = new AddUserLoginModelValidator(identity);
+                var validationResults = validator.Validate(newUserLogin);
+                validationResults.IsValid.ShouldBe(false);
+                validationResults.Errors.Select(x => x.ErrorMessage).ShouldContain("'User Email' is not a valid email address.");
+            });
+        }
+
+        [Test]
+        public void ShouldNotAddUserIfEmailNotUnique()
+        {
+            ResetUsers();
+
+            var existingUser = SetupUsers(1).Single();
+
+            var newUserLogin = new AddUserLoginModel
+            {
+                UserEmail = existingUser.Email,
+                LoginProvider = "test_oidc",
+                ProviderKey = "test_provider_key"
+            };
+
+            Scoped<AdminAppIdentityDbContext>(identity =>
+            {
+                var validator = new AddUserLoginModelValidator(identity);
+                var validationResults = validator.Validate(newUserLogin);
+                validationResults.IsValid.ShouldBe(false);
+                validationResults.Errors.Select(x => x.ErrorMessage).ShouldContain("A user with this email address already exists in the database.");
+            });
+        }
+
+        [Test]
+        public async Task ShouldNotAddUserLoginIfItAlreadyExists()
+        {
+            var existingUserLogin = new AddUserLoginModel
+            {
+                UserEmail = "test@test.com",
+                LoginProvider = "test_oidc",
+                ProviderDisplayName = "test_oidc",
+                ProviderKey = "test_provider_key"
+            };
+
+            await ScopedAsync<AdminAppIdentityDbContext>(async context =>
+            {
+                var command = new AddUserLoginCommand(context);
+
+                var identityUserId = await command.Execute(existingUserLogin);
+            });
+
+            var newUserLogin = new AddUserLoginModel
+            {
+                UserEmail = existingUserLogin.UserEmail,
+                LoginProvider = "test_oidc",
+                ProviderDisplayName = "test_oidc",
+                ProviderKey = "test_provider_key"
+            };
+
+            Scoped<AdminAppIdentityDbContext>(identity =>
+            {
+                var validator = new AddUserLoginModelValidator(identity);
+                var validationResults = validator.Validate(newUserLogin);
+                validationResults.IsValid.ShouldBe(false);
+                validationResults.Errors.Select(x => x.ErrorMessage).ShouldContain("A user with this email address already exists in the database.");
+            });
+        }
+    }
+}
---
+++ Application/EdFi.Ods.AdminApp.Management.Tests/User/OpenIdConnectLoginServiceTests.cs
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Linq;
+using System.Threading.Tasks;
+using EdFi.Ods.AdminApp.Management.Database;
+using EdFi.Ods.AdminApp.Management.Database.Models;
+using EdFi.Ods.AdminApp.Management.User;
+using EdFi.Ods.AdminApp.Web.Helpers;
+using NUnit.Framework;
+using Shouldly;
+using static EdFi.Ods.AdminApp.Management.Tests.Testing;
+using static EdFi.Ods.AdminApp.Management.Tests.User.UserTestSetup;
+
+namespace EdFi.Ods.AdminApp.Management.Tests.User
+{
+    public class OpenIdConnectLoginServiceTests : AdminAppDataTestBase
+    {
+        private const string OidcUserId = "testUserId";
+        private const string OidcUserEmail = "test@test.com";
+        private const string LoginProvider = "oidc";
+        private const string ProviderDisplayName = "oidc";
+
+        [SetUp]
+        public void Init()
+        {
+            EnsureZeroUsers();
+            EnsureZeroUserLogins();
+            EnsureDefaultRoles();
+        }
+
+        [Test]
+        public async Task ShouldAddUserAndUserLoginOnFirstTimeLogin()
+        {
+            var userId = await AddUserLogin(OidcUserId, OidcUserEmail, LoginProvider, ProviderDisplayName);
+
+            var addedUser = Query(userId);
+            addedUser.Email.ShouldBe(OidcUserEmail);
+            addedUser.UserName.ShouldBe(OidcUserEmail);
+            addedUser.PasswordHash.ShouldBeNull();
+            addedUser.RequirePasswordChange.ShouldBe(false);
+
+            Scoped<AdminAppIdentityDbContext>(context =>
+            {
+                var addedUserLogin = context.UserLogins.SingleOrDefault(x => x.UserId == userId);
+                addedUserLogin.ShouldNotBeNull();
+                addedUserLogin.ProviderKey.ShouldBe(OidcUserId);
+                addedUserLogin.LoginProvider.ShouldBe(LoginProvider);
+                addedUserLogin.ProviderDisplayName.ShouldBe(ProviderDisplayName);
+            });
+        }
+
+        [Test]
+        public async Task ShouldAssignSuperAdminRole()
+        {
+            var userId = await AddUserLogin(OidcUserId, OidcUserEmail, LoginProvider, ProviderDisplayName);
+
+            Scoped<AdminAppIdentityDbContext>(context =>
+            {
+                var addedUserRole = context.UserRoles.SingleOrDefault(x => x.UserId == userId);
+                addedUserRole.ShouldNotBeNull();
+                addedUserRole.RoleId.ShouldBe(Role.SuperAdmin.Value.ToString());
+            });
+        }
+
+        private static async Task<string> AddUserLogin(string oidcUserId, string oidcUserEmail, string loginProvider, string providerDisplayName)
+        {
+            string identityUserId = null;
+            await ScopedAsync<AdminAppIdentityDbContext>(
+                async identity =>
+                {
+                    var addUserLoginCommand = new AddUserLoginCommand(identity);
+                    var editUserRoleCommand = new EditUserRoleCommand(identity);
+
+                    var openIdConnectLoginService = new OpenIdConnectLoginService(
+                        addUserLoginCommand, editUserRoleCommand, identity);
+
+                    identityUserId = await openIdConnectLoginService.AddUserLoginForOpenIdConnect(
+                        oidcUserId, oidcUserEmail, loginProvider, providerDisplayName);
+
+                    openIdConnectLoginService.AddSuperAdminRoleToUser(identityUserId);
+                });
+            return identityUserId;
+        }
+
+        private static void EnsureZeroUsers()
+        {
+            Scoped<AdminAppIdentityDbContext>(database =>
+            {
+                foreach (var entity in database.Users)
+                    database.Users.Remove(entity);
+                database.SaveChanges();
+            });
+        }
+
+        private static void EnsureZeroUserLogins()
+        {
+            Scoped<AdminAppIdentityDbContext>(database =>
+            {
+                foreach (var entity in database.UserLogins)
+                    database.UserLogins.Remove(entity);
+                database.SaveChanges();
+            });
+        }
+    }
+}
