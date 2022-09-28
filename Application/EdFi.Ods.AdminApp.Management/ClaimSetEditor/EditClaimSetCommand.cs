0c47900c306770a904f172a3eb7d2eb8d64be421
saa14 <saagar.minocha@accenture.com>
2022-09-29 00:14:06 +0530

    Added logic to re-associate applications to the renamed claimset.

--- Application/EdFi.Ods.AdminApp.Management/ClaimSetEditor/EditClaimSetCommand.cs
+++ Application/EdFi.Ods.AdminApp.Management/ClaimSetEditor/EditClaimSetCommand.cs
@@ -4,27 +4,45 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System.Linq;
+using EdFi.Admin.DataAccess.Contexts;
 using EdFi.Security.DataAccess.Contexts;
 
 namespace EdFi.Ods.AdminApp.Management.ClaimSetEditor
 {
     public class EditClaimSetCommand
     {
-        private readonly ISecurityContext _context;
+        private readonly ISecurityContext _securityContext;
+        private readonly IUsersContext _usersContext;
 
-        public EditClaimSetCommand(ISecurityContext context)
+        public EditClaimSetCommand(ISecurityContext securityContext, IUsersContext usersContext)
         {
-            _context = context;
+            _securityContext = securityContext;
+            _usersContext = usersContext;
         }
 
         public int Execute(IEditClaimSetModel claimSet)
         {
-            var existingClaimSet = _context.ClaimSets.Single(x => x.ClaimSetId == claimSet.ClaimSetId);
+            var existingClaimSet = _securityContext.ClaimSets.Single(x => x.ClaimSetId == claimSet.ClaimSetId);
             existingClaimSet.ClaimSetName = claimSet.ClaimSetName;
 
-            _context.SaveChanges();
+            ReAssociateApplicationsToRenamedClaimSet(existingClaimSet.ClaimSetName, claimSet.ClaimSetName);
+
+            _securityContext.SaveChanges();
 
             return existingClaimSet.ClaimSetId;
+        }
+
+        private void ReAssociateApplicationsToRenamedClaimSet(string existingClaimSetName, string newClaimSetName)
+        {
+            var associatedApplications = _usersContext.Applications
+                .Where(x => x.ClaimSetName == existingClaimSetName);
+
+            foreach (var application in associatedApplications)
+            {
+                application.ClaimSetName = newClaimSetName;
+            }
+
+            _usersContext.SaveChanges();
         }
     }
 
