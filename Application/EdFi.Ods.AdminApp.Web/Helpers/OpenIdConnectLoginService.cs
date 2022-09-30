ab8d3ad7293ec655d0397e8a97592e2a5baf2d61
saa14 <saagar.minocha@accenture.com>
2022-10-01 00:13:59 +0530

    Refactored OpenIdConnectLoginService to add GetIdentityUserIdForOpenIdConnectUser to separate out the check for an existing user record. Added logger to log validation errors as warnings while trying to create the user record.

--- Application/EdFi.Ods.AdminApp.Web/Helpers/OpenIdConnectLoginService.cs
+++ Application/EdFi.Ods.AdminApp.Web/Helpers/OpenIdConnectLoginService.cs
@@ -8,12 +8,15 @@ using EdFi.Ods.AdminApp.Management.Database;
 using EdFi.Ods.AdminApp.Management.Database.Models;
 using EdFi.Ods.AdminApp.Management.User;
 using EdFi.Ods.AdminApp.Web.Models.ViewModels.User;
+using log4net;
 
 namespace EdFi.Ods.AdminApp.Web.Helpers
 {
     public interface IOpenIdConnectLoginService
     {
         Task<string> AddUserLoginForOpenIdConnect(string oidcUserId, string oidcUserEmail, string loginProvider, string providerDisplayName);
+
+        string GetIdentityUserIdForOpenIdConnectUser(string oidcUserId, string loginProvider);
 
         void AddSuperAdminRoleToUser(string identityUserId);
     }
@@ -22,12 +25,15 @@ namespace EdFi.Ods.AdminApp.Web.Helpers
     {
         private readonly AddUserLoginCommand _addUserLoginCommand;
         private readonly EditUserRoleCommand _editUserRoleCommand;
+        private readonly GetUserLoginQuery _getUserLoginQuery;
         private readonly AdminAppIdentityDbContext _identity;
+        private readonly ILog _logger = LogManager.GetLogger(typeof(OpenIdConnectLoginService));
 
-        public OpenIdConnectLoginService(AddUserLoginCommand addUserLoginCommand, EditUserRoleCommand editUserRoleCommand, AdminAppIdentityDbContext identity)
+        public OpenIdConnectLoginService(AddUserLoginCommand addUserLoginCommand, EditUserRoleCommand editUserRoleCommand, GetUserLoginQuery getUserLoginQuery, AdminAppIdentityDbContext identity)
         {
             _addUserLoginCommand = addUserLoginCommand;
             _editUserRoleCommand = editUserRoleCommand;
+            _getUserLoginQuery = getUserLoginQuery;
             _identity = identity;
         }
 
@@ -42,13 +48,49 @@ namespace EdFi.Ods.AdminApp.Web.Helpers
             };
 
             var userLoginModelValidator = new AddUserLoginModelValidator(_identity);
-            var userLoginModelIsValid = (await userLoginModelValidator.ValidateAsync(addUserLoginModel)).IsValid;
+            var userLoginModelValidationResults = (await userLoginModelValidator.ValidateAsync(addUserLoginModel));
 
             string identityUserId = null;
-            if (userLoginModelIsValid)
+            if (userLoginModelValidationResults.IsValid)
             {
                 identityUserId = await _addUserLoginCommand.Execute(addUserLoginModel);
             }
+            else
+            {
+                foreach (var error in userLoginModelValidationResults.Errors)
+                {
+                    _logger.Warn(error.ErrorMessage);
+                }
+            }
+
+            return identityUserId;
+        }
+
+        public string GetIdentityUserIdForOpenIdConnectUser(string oidcUserId, string loginProvider)
+        {
+            var getUserLoginModel = new GetUserLoginModel
+            {
+                LoginProvider = loginProvider,
+                ProviderKey = oidcUserId
+            };
+
+            var userLoginModelValidator = new GetUserLoginModelValidator(_identity);
+            var userLoginModelValidationResults = userLoginModelValidator.Validate(getUserLoginModel);
+
+            string identityUserId = null;
+            if (userLoginModelValidationResults.IsValid)
+            {
+                var identityUserLogin = _getUserLoginQuery.Execute(getUserLoginModel);
+                identityUserId = identityUserLogin.UserId;
+            }
+            else
+            {
+                foreach (var error in userLoginModelValidationResults.Errors)
+                {
+                    _logger.Warn(error.ErrorMessage);
+                }
+            }
+
             return identityUserId;
         }
 
