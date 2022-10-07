0d63c32235a3ea88f557364a0681f72c118e5682
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2022-10-06 20:23:10 -0500

    [AA-1591] Rebuild Actions Workflows (#349)
    * Rename files
    
    * New workflow to run on merge or special tag
    
    * Update the build process on-prerelease
    
    * Update the workflow on-pullrequest
    
    * Use 2 spaces for YML files
    
    * Automate the release process
    
    * Upload test results after PR run
    
    * Install MinVer directly on the projects
    
    * Suppress some PowerShell linting
    
    * Replace Write-Host with Write-Output
    
    * Save as UTF-8 with BOM
    
    * Capture C# unit test output
    
    * Suppress annoying NuGet warning
    
    * Rename methods so that they are singular, not plural
    
    * Ignore unit test results file
    
    * Use Python-like file header for YML
    
    * Remove buildCounter
    
    * Remove TeamCity integration
    
    * Use better syntax for same goal
    
    * Fix a few workflow names
    
    * Review comments
    
    * Fixup the suppression messages
    
    * Remove PowerShell linting

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: Apache-2.0
+﻿# SPDX-License-Identifier: Apache-2.0
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
@@ -242,6 +242,8 @@ function RunTests {
     }
 
     $testAssemblies | ForEach-Object {
+        Write-Output "Executing: dotnet test $($_)"
+        Invoke-Execute { dotnet test $_ /logger:"trx;LogFileName=unit-tests.trx"}
         Write-Output "Executing: dotnet test $($_)"
         Invoke-Execute {
             dotnet test $_ `
