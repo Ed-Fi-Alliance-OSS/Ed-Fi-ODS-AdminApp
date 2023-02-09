0b453fb598c4f709fa4b909cca581483dbb8fd94
CSR2017 <34072978+CSR2017@users.noreply.github.com>
2023-02-09 11:35:32 -0600

    [AA-1645] Update db-deploy version (#381)
    * Update db-deploy version
    
    * to trigger code QL check

--- eng/database-manager.psm1
+++ eng/database-manager.psm1
@@ -6,7 +6,7 @@
 #requires -version 5
 
 $ErrorActionPreference = "Stop"
-Set-Variable DbDeployVersion -option Constant -value "2.3.10068"
+Set-Variable DbDeployVersion -option Constant -value "3.1.24"
 
 Import-Module -Name "$PSScriptRoot/connection-strings.psm1"
 
