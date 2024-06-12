e502b12267791ea91a195a150a84ee005ff9004b
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2024-06-12 11:44:02 -0600

    [AA-1744]  NET 8 - Update Restsharp to latest version (#466)
    * [AA-1744]  NET 8 - Update Restsharp to latest version
    
    - Update  package version.
    - Update code and test to match the new version.
    
    * Update Edfi.SecurityCompatibility53.DataAccess package

--- Application/EdFi.Ods.AdminApp.Management/Api/IOdsRestClient.cs
+++ Application/EdFi.Ods.AdminApp.Management/Api/IOdsRestClient.cs
@@ -1,14 +1,16 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using RestSharp;
 using System.Collections.Generic;
 
 namespace EdFi.Ods.AdminApp.Management.Api
 {
     public interface IOdsRestClient
     {
+        IRestClient RestClient { get; }
         IReadOnlyList<T> GetAll<T>(string elementPath) where T : class;
         IReadOnlyList<T> GetAll<T>(string elementPath, int offset, int limit) where T : class;
         IReadOnlyList<T> GetAll<T>(string elementPath, Dictionary<string, object> filters) where T : class;
