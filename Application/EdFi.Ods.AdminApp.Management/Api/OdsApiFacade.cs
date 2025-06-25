f760725829fbdcd36518410644fd2ea1c2ae56f3
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-06-25 13:53:13 -0600

    Update OdsApiFacade.cs
    Remove unused variables

--- Application/EdFi.Ods.AdminApp.Management/Api/OdsApiFacade.cs
+++ Application/EdFi.Ods.AdminApp.Management/Api/OdsApiFacade.cs
@@ -41,7 +41,6 @@ namespace EdFi.Ods.AdminApp.Management.Api
         public List<Models.School> GetSchoolsByLeaIds(IEnumerable<int> leaIds)
         {
             var response = new List<School>();
-            int n = 0;
             foreach (var leaId in leaIds)
             {
                 var filters = new Dictionary<string, object>
