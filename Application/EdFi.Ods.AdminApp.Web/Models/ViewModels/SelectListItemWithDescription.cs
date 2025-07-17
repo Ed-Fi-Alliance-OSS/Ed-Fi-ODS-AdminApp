34f5f5c3623b3f90d5a8d5a6993b8c8edca6a9fb
Danny Fernandez A <129120407+dfernandez-gap@users.noreply.github.com>
2025-07-17 16:30:54 -0600

    [AA-1802] Fix Options Display Long Descriptions That Overflow For Post-Secondary Institution Level (#521)
    * Extended functionality to manage short description field
    
    * Fix spacing issues

--- Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/SelectListItemWithDescription.cs
+++ Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/SelectListItemWithDescription.cs
@@ -3,13 +3,12 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-namespace EdFi.Ods.AdminApp.Management.Api.DomainModels
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EdFi.Ods.AdminApp.Web.Models.ViewModels
 {
-    public class EdFiDescriptor
+    public class SelectListItemWithDescription : SelectListItem
     {
-        public string CodeValue { get; set; }
-        public string Namespace { get; set; }
-        public string Description { get; set; }
-        public string ShortDescription { get; set; }
+        public string Description { get; set; } = string.Empty;
     }
 }
