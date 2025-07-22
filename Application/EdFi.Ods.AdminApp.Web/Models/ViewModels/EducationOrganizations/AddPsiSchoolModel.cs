ed93807a6c4c96f24c1a0f70bd30bf92aaa49cc4
David Jimenez <djimenez@growthaccelerationpartners.com>
2025-07-22 16:37:44 -0600

    Adds maxlength attribute on Educator and School screens - Includes Schools as well

--- Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/EducationOrganizations/AddPsiSchoolModel.cs
+++ Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/EducationOrganizations/AddPsiSchoolModel.cs
@@ -5,6 +5,7 @@
 
 using System.Collections.Generic;
 using EdFi.Ods.AdminApp.Management.Api.Models;
+using FluentValidation;
 
 namespace EdFi.Ods.AdminApp.Web.Models.ViewModels.EducationOrganizations
 {
@@ -19,5 +20,35 @@ namespace EdFi.Ods.AdminApp.Web.Models.ViewModels.EducationOrganizations
 
     public class AddPsiSchoolModelValidator : AddSchoolModelValidatorBase<AddPsiSchoolModel>
     {
+        public AddPsiSchoolModelValidator()
+        {
+            RuleFor(m => m.LocalEducationAgencyId).NotEmpty();
+
+            RuleFor(m => m.Name).NotEmpty();
+            RuleFor(m => m.Name)
+                .MaximumLength(75)
+                .WithMessage("The School Name would be too long for Admin App. The maximum length is 75 characters.")
+                .When(x => x.Name != null);
+
+            RuleFor(m => m.StreetNumberName).NotEmpty();
+            RuleFor(m => m.StreetNumberName)
+                .MaximumLength(150)
+                .WithMessage("The Address would be too long for Admin App. The maximum length is 150 characters.")
+                .When(x => x.StreetNumberName != null);
+
+            RuleFor(m => m.State).NotEmpty();
+
+            RuleFor(m => m.City).NotEmpty();
+            RuleFor(m => m.City)
+                .MaximumLength(30)
+                .WithMessage("The City would be too long for Admin App. The maximum length is 30 characters.")
+                .When(x => x.City != null);
+
+            RuleFor(m => m.ZipCode).NotEmpty();
+            RuleFor(m => m.ZipCode)
+                .MaximumLength(17)
+                .WithMessage("The Zip Code would be too long for Admin App. The maximum length is 17 characters.")
+                .When(x => x.ZipCode != null);
+        }
     }
 }
