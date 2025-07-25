786ec13ae06570175deb8c011c30f61a0eb63757
Jesus Pardo <jpardo@growthaccelerationpartners.com>
2025-07-25 15:07:19 -0400

    Release Admin App

--- EdFi.Suite3.Installer.AdminApp/upgrade.ps1
+++ EdFi.Suite3.Installer.AdminApp/upgrade.ps1
@@ -19,7 +19,7 @@ $adminAppSource = "$packageSource/AdminApp"
 
 $p = @{
     ToolsPath = "C:/temp/tools"
-    PackageVersion = '3.2.1.0'
+    PackageVersion = '3.3.2.0'
     PackageSource = $adminAppSource
 }
 
